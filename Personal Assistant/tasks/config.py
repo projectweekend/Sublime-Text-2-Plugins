3fbf8efc0b0c3b803c6c57148323a6596fbe3a15
Brian Hines <brian@projectweekend.net>
2014-09-10 16:17:15 -0500

    Add a config file

---
+++ Personal Assistant/tasks/config.py
@@ -0,0 +1,1 @@
+JS_SCRATCH_FILE = '/Users/brian/Desktop/scratch_js/scratch.js'
--- Personal Assistant/tasks/js.py
+++ Personal Assistant/tasks/js.py
@@ -1,14 +1,9 @@
-import os
-
-import sublime_plugin
-
-
-SCRATCH_FILE = '/Users/brian/Desktop/scratch_js/scratch.js'
+from config import JS_SCRATCH_FILE
 
 
 class PersonalAssistantTask(object):
 
     def __init__(self, sublime_api):
         self._api = sublime_api
-        view = self._api.window.open_file(SCRATCH_FILE)
+        view = self._api.window.open_file(JS_SCRATCH_FILE)
         self._api.window.focus_view(view)
